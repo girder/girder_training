04b5f3c50be3f10c91aa584c710b32278a82c4f4
Patrick Reynolds <patrick.reynolds@kitware.com>
2016-04-03 15:18:17 -0400

    Adding an example script for Anisotropic Diffusion.

---
+++ blur_example/scripts/cad_script.py
@@ -0,0 +1,42 @@
+import itk
+
+InputPixelType = itk.F
+OutputPixelType = itk.UC
+Dimensions = 2
+
+numberOfIterations = 50
+timeStep = 0.1
+conductance = 0.4
+
+inputFileName = '../../BrainProtonDensitySlice.png'
+outputFileName = 'out.png'
+
+outputPixelTypeMin = itk.NumericTraits[OutputPixelType].min()
+outputPixelTypeMax = itk.NumericTraits[OutputPixelType].max()
+
+InputImageType = itk.Image[InputPixelType, Dimensions]
+OutputImageType = itk.Image[OutputPixelType, Dimensions]
+ReaderType = itk.ImageFileReader[InputImageType]
+FilterType = itk.CurvatureAnisotropicDiffusionImageFilter[InputImageType, InputImageType]
+RescalerType = itk.RescaleIntensityImageFilter[InputImageType, OutputImageType]
+WriterType = itk.ImageFileWriter[OutputImageType]
+
+reader = ReaderType.New()
+reader.SetFileName(inputFileName)
+
+cadFilter = FilterType.New()
+cadFilter.SetInput(reader.GetOutput())
+cadFilter.SetNumberOfIterations(numberOfIterations)
+cadFilter.SetTimeStep(timeStep)
+cadFilter.SetConductanceParameter(conductance)
+
+rescalar = RescalerType.New()
+rescalar.SetInput(cadFilter.GetOutput())
+rescalar.SetOutputMinimum(outputPixelTypeMin)
+rescalar.SetOutputMaximum(outputPixelTypeMax)
+
+writer = WriterType.New()
+writer.SetFileName(outputFileName)
+writer.SetInput(rescalar.GetOutput())
+writer.Update()
+
