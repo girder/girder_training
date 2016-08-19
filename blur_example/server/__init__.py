e1e3259fd94aeb622b71ca39e0e55014562b2e31
mgrauer <michael.grauer@kitware.com>
2016-08-19 16:48:26 +0000

    Add simple python add endpoint

--- blur_example/server/__init__.py
+++ blur_example/server/__init__.py
@@ -13,6 +13,71 @@ class Blur(Resource):
 
         self.resourceName = 'blur_example'
         self.route('POST', (':id',), self.createBlurImage)
+        self.route('POST', ('simple_add',), self.simpleAdd)
+
+    @access.user
+    @filtermodel(model='job', plugin='jobs')
+    @describeRoute(
+        Description('Simple pure python addition using the worker.')
+        .param('a', 'First param to be added.',
+               dataType='integer', required=True)
+        .param('b', 'Second param to be added.',
+               dataType='integer', required=True)
+    )
+    def simpleAdd(self, params):
+        user = self.getCurrentUser()
+        token = self.getCurrentToken()
+        jobTitle = 'Simple Python Addition'
+        jobModel = self.model('job', 'jobs')
+
+        job = jobModel.createJob(
+            title=jobTitle, type='python_add', handler='worker_handler',
+            user=user)
+        jobToken = jobModel.createJobToken(job)
+
+        scriptFile = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                  'scripts', 'add_script.py')
+        with open(scriptFile, 'r') as fh:
+            script = fh.read()
+
+        kwargs = {
+            'task': {
+                'name': 'add',
+                'inputs': [
+                    {
+                        'name': 'a',
+                        'type': 'number',
+                        'format': 'number',
+                        'data': int(params.get('a', 0))
+                    },
+                    {
+                        'name': 'b',
+                        'type': 'number',
+                        'format': 'number',
+		        'data': int(params.get('b', 1))
+                    }
+                ],
+                'outputs': [{'name': 'c', 'type': 'number', 'format': 'number'}],
+                'script': script,
+                'mode': 'python'
+            },
+            'inputs': {
+                'a': { 'format': 'number', 'data':  int(params.get('a', 0))},
+                'b': { 'format': 'number', 'data':  int(params.get('b', 1))}
+            },
+            'jobInfo': {
+                'method': 'PUT',
+                'url': '/'.join((getApiUrl(), 'job', str(job['_id']))),
+                'headers': {'Girder-Token': jobToken['_id']},
+                'logPrint': True
+            },
+            'validate': False,
+            'auto_convert': False
+        }
+        job['kwargs'] = kwargs
+        job = jobModel.save(job)
+        jobModel.scheduleJob(job)
+        return job
 
     @access.user
     @loadmodel(model='item', level=AccessType.WRITE)
